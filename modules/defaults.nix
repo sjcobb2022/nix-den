7ef9d19abde91429bba0d452d9b36fb5318d7bfc
Samuel Cobb <sjcobb2003@gmail.com>
2026-04-30 14:22:46 +0100

    darwin v

--- modules/defaults.nix
+++ modules/defaults.nix
@@ -6,6 +6,7 @@
 }: {
   den.default.nixos.system.stateVersion = "25.11";
   den.default.homeManager.home.stateVersion = "25.11";
+  den.default.darwin.stateVersion = "25.11";
 
   den.default.nixos.nix = {
     settings = {
