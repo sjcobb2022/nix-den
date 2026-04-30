a1b27e4fb75b72095ef172ef31a8b1a285efe7b0
Samuel Cobb <sjcobb2003@gmail.com>
2026-04-30 13:18:41 +0100

    update darwin input

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 # DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
 # Use `nix run .#write-flake` to regenerate it.
 {
-  outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
     den.url = "github:vic/den/feat/fx-pipeline";
